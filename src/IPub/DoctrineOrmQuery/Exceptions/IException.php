e9980f549d7e7b7a0cb2164627297ef2c1bc76af
Adam Kadlec <adam.kadlec@gmail.com>
2019-11-11 00:08:56 +0100

    - Repo init

---
+++ src/IPub/DoctrineOrmQuery/Exceptions/IException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * IException.php
+ *
+ * @copyright      More in license.md
+ * @license        https://www.fastybird.com
+ * @author         Adam Kadlec <adam.kadlec@fastybird.com>
+ * @package        iPublikuj:DoctrineOrmQuery!
+ * @subpackage     Exceptions
+ * @since          1.0.0
+ *
+ * @date           10.11.19
+ */
+
+declare(strict_types = 1);
+
+namespace IPub\DoctrineOrmQuery\Exceptions;
+
+interface IException
+{
+}
---
+++ src/IPub/DoctrineOrmQuery/Exceptions/InvalidStateException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * InvalidStateException.php
+ *
+ * @copyright      More in license.md
+ * @license        https://www.fastybird.com
+ * @author         Adam Kadlec <adam.kadlec@fastybird.com>
+ * @package        iPublikuj:DoctrineOrmQuery!
+ * @subpackage     Exceptions
+ * @since          1.0.0
+ *
+ * @date           10.11.19
+ */
+
+declare(strict_types = 1);
+
+namespace IPub\DoctrineOrmQuery\Exceptions;
+
+class InvalidStateException extends \RuntimeException implements IException
+{
+}
